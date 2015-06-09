723af355d42fc700e53f2a617a9f38603583d6f6
Christina Davies <cc.goulet@gmail.com>
2015-06-09 15:41:20 +0900

    commit message

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+  	$('.ryu-still').hide();
+  	$('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+  	$('.ryu-ready').hide();
+  	$('.ryu-still').show();
+  })
+  .mousedown(function() {
+  	playHadouken();
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').finish().show()
+  	.animate(
+  		{'left': '1020px'},
+  		500,
+  		function() {
+  			$(this).hide();
+  			$(this).css('left', '520px');
+  		});
+  })
+  .mouseup(function() {
+  	$('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  });
+  $(document).keydown(function(e) {
+  	var a = 88;
+  	if (e.keyCode == a){
+    $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-cool').show();
+    }
+  })
+  .keyup(function() {
+  	$('.ryu-cool').hide();
+  	$('.ryu-still').show();
+  });
+});
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
